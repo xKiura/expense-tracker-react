49df322d1cad54a4e612dd272e35e5fe1d2d8d80
xKiura <63990468+xKiura@users.noreply.github.com>
2022-10-29 23:45:58 +0300

    added database/Express API

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const transactions = require('./routes/transactions');
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'developement') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/transactions', transactions);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
